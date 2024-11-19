70aa6ed282fb7aa2659a5b05a3a24382d848b3b1
Aayan Aqdas <80333428+aayanaqdas@users.noreply.github.com>
2024-11-19 13:26:13 +0100

    programmet er case insensitive og la inn en exit funksjon. gjort at man kan søke etter setning er eller flere ord

--- main.py
+++ main.py
@@ -5,20 +5,31 @@ def read_file():
     for file in filenames: # Loop through each filename in the filenames list.
         with open(file, "r", encoding="utf-8") as f: # Open the current file in read mode with UTF-8 encoding.
             text += f.read() # Read the content of the file and append it to the text string
-            text_list = [line for line in text.splitlines()] # Split the text string into a list of lines
+            text_list = text.splitlines() # Split the text string into a list of lines
     return text_list
 
-def findLine():
+def findLine(word):
     text = read_file() # reads the text from the files
-    while True: #infinite loop
-        word = input("Enter a word: ") # takes the word from userinput
-        found = False
-        for line in text:
-            if word in line.split(): #if word is in the text line
-                print(line)
-                found = True
+    found = False
+    word_count = 0
+    word_lower = word.lower()
+    for line in text:
+        if word_lower in line.lower():
+            print(line)
+            found = True
+            word_count += 1
+    print(f"The word '{word}' was found {word_count} times)")
+    if not found:
+        print("Word not found")
 
-        if not found:
-            print("Word not found")
 
-findLine()
+
+
+def main():
+    while True: # loop until user exits
+        word = input("Enter a word (4 for exit): ") # takes the word from userinput
+        if word == "4":
+            break
+        findLine(word)
+
+main()
