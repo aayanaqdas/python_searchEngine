31c8d37e124593e2d9d18e145c360501a010dcb7
Aayan Aqdas <80333428+aayanaqdas@users.noreply.github.com>
2024-11-19 11:41:39 +0100

    find word function.

--- main.py
+++ main.py
@@ -5,7 +5,19 @@ def read_file():
     for file in filenames: # Loop through each filename in the filenames list.
         with open(file, "r", encoding="utf-8") as f: # Open the current file in read mode with UTF-8 encoding.
             text += f.read() # Read the content of the file and append it to the text string
-            text_list = [line for line in text.splitlines()]
-            
-    return text_list 
+            text_list = [line for line in text.splitlines()] # Split the text string into a list of lines
+    return text_list
 
+def findWord():
+    text = read_file() # reads the text from the files
+    while True: #infinite loop
+        word = input("Enter a word: ") # takes the word from userinput
+        found = False
+        for line in text:
+            if word in line: #if word is in the text line
+                print(line)
+                found = True
+        if not found:
+            print("Word not found")
+
+findWord()
